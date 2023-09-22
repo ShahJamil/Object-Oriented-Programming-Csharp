c3a4a2e9c074d07dbf7cb49b39ab68df8e62d789
ShahJamil <145757585+ShahJamil@users.noreply.github.com>
2023-09-22 23:09:58 +0400

    Add files via upload

---
+++ Matrix Calculator Project/CdwcrzHW/Diagonals.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CdwcrzHW
+{
+    public class Diagonals
+    {
+        #region Exceptions
+        public class NegativeSizeException : Exception { };
+        public class ReferenceToNullPartException : Exception { };
+        public class DifferentSizeException : Exception { };
+        #endregion
+
+
+        public List<double> vector = new List<double>();
+
+        public Diagonals(int size)
+        {
+            if (size % 2 == 0)
+            {
+                if (size <= 0) throw new NegativeSizeException();
+                else
+                {
+                    for (int i = 0; i < size * 2; i++)
+                    {
+                        vector.Add(0);
+                    }
+
+                }
+            }
+            else
+            {
+                if (size < 0) throw new NegativeSizeException();
+                else
+                {
+                    for (int i = 0; i < size * 2 - 1; i++)
+                    {
+                        vector.Add(0);
+                    }
+
+                }
+            }
+
+        }
+        public Diagonals(Diagonals d)
+        {
+            for (int i = 0; i < d.vector.Count; ++i)
+            {
+                vector.Add(d.vector[i]);
+            }
+        }
+
+        public int Size // Property for getting the size of the matrix
+        {
+            get { return vector.Count; }
+        }
+        public double this[int i, int j]
+        {
+            
+            get
+            {
+                
+                if (Size % 2 == 0)
+                {
+                    if (i < 0 || i >= (Size) / 2 || j < 0 | j >= (Size) / 2)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    if (i == j)
+                    {
+                        return vector[i];
+                    }
+                    else if (i + j + 1 == Size / 2)
+                    {
+                        return vector[Size / 2 + i];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                  
+                    if (i < 0 || i >= (Size) / 2 + 1 || j < 0 | j >= (Size) / 2 + 1)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    if (i == j)
+                    {
+                        return vector[i];
+                        
+                    }
+                    else if (i + j == Size / 2 && i != j)
+                    {
+                        
+                        if (i > j)
+                        {
+                            return vector[Size / 2 + i];
+                        }
+                        else if(i < j)
+                        {
+                            return vector[Size / 2 + 1 + i];
+                        }
+                    }
+                    
+                    return 0;
+                }
+
+            }
+            set
+            {
+                if (Size % 2 == 0)
+                {
+                    if (i < 0 || i >= (Size) / 2 || j < 0 | j >= (Size) / 2)
+                    {   
+                        throw new IndexOutOfRangeException();
+                    }
+                    if (i == j)
+                    {
+                        vector[i] = value;
+                    }
+                    else if (i + j + 1 == Size / 2)
+                    {
+                        vector[Size / 2 + i] = value;
+                    }
+                    else
+                    {
+                        throw new ReferenceToNullPartException();
+                    }
+                    
+                }
+                else
+                {
+                    if (i < 0 || i >= (Size) / 2 + 1 || j < 0 | j >= (Size) / 2 + 1)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    if (i == j)
+                    {
+                        vector[i] = value;
+                    }
+                    else if (i + j == Size / 2 && i != j)
+                    {
+
+                        if (i > j)
+                        {
+                            vector[Size / 2 + i] = value ;
+                        }
+                        else if (i < j)
+                        {
+                             vector[Size / 2 + 1 + i] = value;
+                        }
+                    }
+                    
+
+
+                }
+
+            }
+        }
+
+        public void Set(in List<double> x)
+        {
+            if (this.Size != x.Count) throw new DifferentSizeException();
+            for (int i = 0; i < Size; i++)
+            {
+                this.vector[i] = x[i];
+            }
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || !(obj is Diagonals))
+                return false;
+            else
+            {
+                Diagonals diag = obj as Diagonals;
+                if (diag.Size != this.Size) return false;
+                for (int i = 0; i < vector.Count; i++)
+                {
+                    if (vector[i] != diag.vector[i]) return false;
+                }
+                return true;
+            }
+        }
+
+        public static Diagonals operator +(Diagonals a, Diagonals b)
+        {
+            if (a.Size != b.Size) throw new DifferentSizeException();
+            Diagonals c = new Diagonals(a);
+            for (int i = 0; i < c.Size; ++i)
+            {
+                c.vector[i] = a.vector[i] + b.vector[i];
+            }
+            return c;
+        }
+        public static Diagonals operator *(Diagonals a, Diagonals b)
+        {
+            
+            if (a.Size != b.Size) throw new DifferentSizeException();
+            Diagonals c = new Diagonals(a);
+            if (a.Size % 2 == 0)
+            {
+                double res;
+                for (int i = 0; i < a.Size / 2; ++i)
+                {
+                    
+                    for (int j = 0; j < a.Size / 2; ++j)
+                    {
+                        res = 0;
+                        for (int k = 0; k < a.Size / 2; ++k)
+                        {
+                            res += a[i, k] * b[k, j];
+                        }
+                        c[i, j] = res;
+                    }
+                    
+                   
+
+                    
+                }
+                return c;
+            }
+            else
+            {
+
+
+                double res;
+                for (int i = 0; i < a.Size / 2 + 1; ++i)
+                {
+                    
+                    for (int j = 0; j < a.Size / 2 + 1; ++j)
+                    {
+                        res = 0;
+                        for (int k = 0; k < a.Size / 2 + 1; ++k)
+                        {
+                            res += a[i, k] * b[k, j];
+
+                        }
+                        c[i, j] = res;
+
+                    }
+                    
+                }
+                
+                return c;
+            }
+
+
+            
+        }
+
+        public void Print()
+        {
+
+            if (Size % 2 == 0)
+            {
+                for (int i = 0; i < Size / 2; i++)
+                {
+                    for (int j = 0; j < Size / 2; j++)
+                    {
+                        Console.Write($"{this[i, j]} ");
+
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Size / 2 + 1; i++)
+                {
+                    for (int j = 0; j < Size / 2 + 1; j++)
+                    {
+                        Console.Write($"{this[i, j]} ");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Matrix Calculator Project/CdwcrzHW/Menu.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CdwcrzHW
+{
+    public class Menu
+    {
+        private List<Diagonals> vec = new List<Diagonals>();
+
+        public Menu() { }
+
+        public void Run()
+        {
+            int n;
+            do
+            {
+                PrintMenu();
+                try
+                {
+                    n = int.Parse(Console.ReadLine()); // there was !
+                }
+                catch (System.FormatException)
+                {
+                    n = -1;
+                    Console.WriteLine("Enter a digit between 1 and 6.");
+                }
+                switch (n)
+                {
+                    case 1:
+                        GetElement();
+                        break;
+                    case 2:
+                        PrintMatrix();
+                        break;
+                    case 3:
+                        AddMatrix();
+                        break;
+                    case 4:
+                        Sum();
+                        break;
+                    case 5:
+                        Mul();
+                        break;
+                }
+
+            } while (n != 0);
+
+        }
+
+        #region Menu operations
+
+        static private void PrintMenu()
+        {
+            Console.WriteLine("\n\n 0. - Quit");
+            Console.WriteLine(" 1. - Get an element");
+            Console.WriteLine(" 2. - Print a matrix");
+            Console.WriteLine(" 3. - Set a matrix");
+            Console.WriteLine(" 4. - Add matrices");
+            Console.WriteLine(" 5. - Multiply matrices");
+            Console.Write(" Choose: ");
+        }
+
+        private int GetIndex()
+        {
+            if (vec.Count == 0) return -1;
+            int n = 0;
+            bool ok;
+            
+            do
+            {
+                Console.Write("Give a matrix index: ");
+                ok = false;
+                try
+                {
+                    n = int.Parse(Console.ReadLine()); // there was a !
+                    ok = true;
+                    if (n <= 0 || n > vec.Count)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Integer is expected!");
+                }
+
+                catch(ArgumentOutOfRangeException)
+                {
+                    ok = false;
+                    Console.WriteLine("No such matrix!");
+                }
+            } while (!ok);
+            return n - 1;
+        }
+
+        private void GetElement()
+        {
+            if (vec.Count == 0)
+            {
+                Console.WriteLine("Set a matrix first!");
+                return;
+            }
+            int ind = GetIndex();
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Give the index of the row: ");
+                    int i = int.Parse(Console.ReadLine());  // there was !
+                    Console.WriteLine("Give the index of the column: ");
+                    int j = int.Parse(Console.ReadLine());  // there was !
+                    Console.WriteLine($"a[{i},{j}]={vec[ind][i - 1, j - 1]}");
+                    break;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine($"Index must be between 1 and {vec[ind].Size / 2}");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine($"Index must be between 1 and {vec[ind].Size / 2}");
+                }
+            } while (true);
+        }
+
+
+        private void PrintMatrix()
+        {
+            if (vec.Count == 0)
+            {
+                Console.WriteLine("Set a matrix first!");
+                return;
+            }
+            int ind = GetIndex();
+            vec[ind].Print();
+        }
+        private void AddMatrix()
+        {
+            int ind = vec.Count;
+            int n = -1;
+            bool ok = false;
+
+            do
+            {
+                Console.Write("Give size:");
+
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    ok = n > 0;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("The size must be greater than 0.");
+                }
+            } while (!ok);
+
+
+            Diagonals d = new Diagonals(n);
+            ok = true;
+            List<double> elements = new List<double>();
+            if (n % 2 == 0)
+            {
+                for (int i = 0; i < n * 2; i++)
+                {
+                    Console.Write("Element: ");
+                    try
+                    {
+                        double elem = double.Parse(Console.ReadLine()); // ! expected
+                        elements.Add(elem);
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("Number is expected!");
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n * 2 - 1; i++)
+                {
+                    Console.Write("Element: ");
+                    try
+                    {
+                        double elem = double.Parse(Console.ReadLine()); // ! expected
+                        elements.Add(elem);
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("Number is expected!");
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+
+
+            if (ok)
+            {
+                d.Set(elements);
+                vec.Add(d);
+            }
+        }
+
+
+        private void Sum()
+        {
+
+            if (vec.Count == 0)
+            {
+                Console.WriteLine("Set a matrix first!");
+                return;
+            }
+
+            Console.WriteLine("Please give the 1st matrix");
+            int ind1 = GetIndex();
+            Console.WriteLine("Please give the 2nd matrix");
+            int ind2 = GetIndex();
+
+            try
+            {
+                (vec[ind1] + vec[ind2]).Print();
+            }
+            catch (Diagonals.DifferentSizeException)
+            {
+                Console.WriteLine("Matrices must be the same size");
+            }
+        }
+        private void Mul()
+        {
+            if (vec.Count == 0)
+            {
+                Console.WriteLine("Set a matrix first!");
+                return;
+            }
+
+            Console.WriteLine("Please give the 1st matrix");
+            int ind1 = GetIndex();
+            Console.WriteLine("Please give the 2nd matrix");
+            int ind2 = GetIndex();
+
+            try
+            {
+                (vec[ind1] * vec[ind2]).Print();
+
+            }
+            catch (Diagonals.DifferentSizeException)
+            {
+                Console.WriteLine("Matrices must be the same size");
+            }
+        }
+        #endregion
+    }
+}
